25afd9e15ceca430594d31e0fe0db6614549c34e
Mohamad Radisha <pr0ph0z23@gmail.com>
2023-10-06 19:20:12 +0700

    feat(fe): Add login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,29 @@
 import { MantineProvider } from '@mantine/core';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './Login'
 import '@mantine/core/styles.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider>
-      <App />
+      <RouterProvider router={router} />
     </MantineProvider>
   </React.StrictMode>,
 )
