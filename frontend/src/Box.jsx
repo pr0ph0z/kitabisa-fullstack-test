f89ab3d095283ef29fdf7af407175b72241423ee
Mohamad Radisha <pr0ph0z23@gmail.com>
2023-10-06 23:46:43 +0700

    refactor(fe/box): Update styling

--- frontend/src/Box.jsx
+++ frontend/src/Box.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function Box(props) {
   return (
     <div style={{ width: "100px", border: "1px solid black" }}>
-      <h3 style={{ textTransform: "capitalize", margin: "0px" }}>
+      <h3 style={{ textTransform: "uppercase", margin: "0px" }}>
         {props.code}
       </h3>
       <span>{props.rate}</span>
